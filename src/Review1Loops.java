bbfc813976cd10e4e048b5ab937b801af24aa629
gronea <gronea@LR064W7M-1718.ellis.int>
2018-10-01 11:51:37 -0400

    Pseudo code for Loops : )

---
+++ src/Review1Loops.java
@@ -0,0 +1,12 @@
+//Review Assignment 1: Loops
+public class Review1Loops {
+	public static void main(String[] args) {
+		//Determine the destination and nightly rate
+			//3 different hotels
+		
+		//ask how many nights they are staying (loop)
+			//Make sure it is more than 0
+		//Print total cost
+		//Make a bar graph using '*' for every $50
+	}
+}
